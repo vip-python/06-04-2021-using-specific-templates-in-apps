20cc7cfae481e825e530d9b608b7509d4ba1a73b
vip-python <vipvishwa505s@gmail.com>
2021-04-10 11:45:19 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+    return render(request,'samp1.html')
+
+# Create your views here.
---
+++ app2/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app2 import views
+
+app_name='app2'
+urlpatterns = [
+    path('app2/',views.index,name='app2')
+]
